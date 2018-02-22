5cb585993826d3d4f6fa207439870c8db35ea6d9
Benjamín Starý <stary.benjamin@gmail.com>
2018-02-22 19:53:31 +0100

    project clean up, axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  componentWillMount() {
+    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=9').then((res) => {
+      console.log(res);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
